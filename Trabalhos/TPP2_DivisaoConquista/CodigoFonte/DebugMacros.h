4a977f0647202aec7764e27d8c23ef42bb999c32
Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>
2017-11-12 22:51:45 -0200

    Completed TPP2 SourceCode

---
+++ Trabalhos/TPP2_DivisaoConquista/CodigoFonte/DebugMacros.h
@@ -0,0 +1,28 @@
+#ifndef DEBUGMACROS__H
+#define DEBUGMACROS__H
+
+#include <iostream>
+
+#ifdef DEBUG
+    #define DBG_VAR(VAR)\
+        std::cout << "DBG: " << __FILE__ << "(" << __LINE__ << ") "\
+        << #VAR << " = [" << (VAR) << "]" << std::endl
+
+    #define DBG_MSG(MSG)\
+        std::cout << "DBG: " << __FILE__ << "(" << __LINE__ << ") - "\
+        << (MSG) << std::endl    
+    
+    #define DBG_VECTOR(V, S) \
+        do{\
+            printf("Vetor: ");\
+            for(int i = 0; i < (S); ++i)\
+                printf("[%03d] ", V[i]);\
+            printf("\n");\
+        }while(0)
+#else
+    #define DBG_VAR(VAR)
+    #define DBG_MSG(MSG)
+    #define DBG_VECTOR(V, S)
+#endif
+
+#endif //DEBUGMACROS__H
--- Trabalhos/TPP2_DivisaoConquista/CodigoFonte/Main.cpp
+++ Trabalhos/TPP2_DivisaoConquista/CodigoFonte/Main.cpp
@@ -2,13 +2,17 @@
 #include <mpi.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
-#include "SacoDeTrabalho.h"
+//#include "SacoDeTrabalho.h"
 
 // #include "MasterProcess.h"
 // #include "SlaveProcess.h"
 
-#define DEBUG
+#include <iostream>
+
+#include "DebugMacros.h"
+#include <assert.h>
 
 /*
 *   MPI Process tags
@@ -23,10 +27,11 @@ enum enmTagCommand
 *   Global flags
 */
 class globalFlags{
+//private:
 public:
-    static bool quickSortSet = false;
-    static int arraySize = 0;
-    static int delta = 0;
+    static bool quickSortSet;
+    static int arraySize;
+    static int delta;
 
     static bool parseInputArgs(int argc, char **argv, int myRank)
     {
@@ -53,6 +58,58 @@ public:
                     quickSortSet = true;
                 }
             }
+        }
+        return result;
+    }
+};
+//Global flags members initialization
+bool globalFlags::quickSortSet = false;
+int globalFlags::arraySize = 0;
+int globalFlags::delta = 0;
+///////////
+
+/*
+*   Node class
+*/
+class Node
+{
+public:
+    int leftChildRank;
+    int rightChildRank;
+
+    Node(int my_rank, int proc_n)
+    {
+        getChildRank(my_rank, proc_n);
+    }
+
+private:
+    void getChildRank(int my_Rank, int proc_n)
+    {
+        int processNumber = my_Rank + 1; 
+        int myTreeLevel = getNodeLevel(processNumber);
+        int maxTreeLevel = getNodeLevel(proc_n);
+        if(myTreeLevel < maxTreeLevel)
+        {
+            leftChildRank = (my_Rank * 2) + 1;
+            rightChildRank = leftChildRank + 1;
+        }
+        else
+        {
+            //Im a leaf in ths tree, so i don't have childs
+            leftChildRank = rightChildRank = -1;
+        }
+    }
+
+    int getNodeLevel(int processNum)
+    {
+        if(processNum <= 1)
+        {
+            return 0;
+        }
+        else
+        {
+            int level = getNodeLevel(processNum >> 1);
+            return level + 1;
         }
     }
 };
@@ -88,61 +145,78 @@ int compare (const void * a, const void * b)
   return ( *(int*)a - *(int*)b );
 }
 
-initializeVector(int *vector, arraySize)
+/*
+*   Populate root process array with the worst case for sorting  
+*/
+void initializeVector(int *vector, int arraySize)
 {
     for (int i = 0 ; i < arraySize; ++i)              /* init array with worst case for sorting */
         vector[i] = arraySize - i;
     
-        #ifdef DEBUG
-        printf("\nVetor: ");
-        for (int i=0 ; i<arraySize; i++)              /* print unsorted array */
-            printf("[%03d] ", vector[i]);
+    DBG_VECTOR(vector, arraySize);
+}
 
-        printf("\n");
-        #endif
+
+/*
+* recebe um ponteiro para um vetor que contem as mensagens recebidas dos filhos e            
+* intercala estes valores em um terceiro vetor auxiliar. Devolve um ponteiro para este vetor 
+*/
+//int *interleaving(int vetor[], int tam)
+int *interleaving(int *vetor, int tam, int tam_vetor_esquera)
+{
+	int *vetor_auxiliar;
+	int i1, i2, i_aux;
+
+	vetor_auxiliar = new int[tam];//(int *)malloc(sizeof(int) * tam);
+
+	i1 = 0;
+	i2 = tam_vetor_esquera;
+
+	for (i_aux = 0; i_aux < tam; i_aux++) {
+		if (((vetor[i1] <= vetor[i2]) && (i1 < (tam / 2)))
+		    || (i2 == tam))
+			vetor_auxiliar[i_aux] = vetor[i1++];
+		else
+			vetor_auxiliar[i_aux] = vetor[i2++];
+    }
+    
+    delete[] vetor;
+
+	return vetor_auxiliar;
 }
 
 
 int main(int argc, char **argv)
 {
-
-
-
-
     int my_rank;  /* Identificador do processo */
     int proc_n;   /* Número de processos */
     int result = 0; /* Resultado a ser retornado pelo programa */
-    MPI_Status status;
+    double t1, t2; //temporizadores para medir tempo de execução do algoritmo
+    MPI_Status status_pai, status_filhos;
 
-    
-
-    
-    MPI_Init(&argc , & argv);
+    MPI_Init(&argc , &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &proc_n);
 
-    
     //Parse input arguments
-    bool isArgsValid = parseInputArgs(argc, argv, my_rank);
-
+    bool isArgsValid = globalFlags::parseInputArgs(argc, argv, my_rank);
 
     if(isArgsValid)
     {
-        int *vetor;           //pointer to data vector
-        int tam_vetor = 0;    //Vector size will be different for each process
-        if(myRank != 0)
+        int tam_vetor = globalFlags::arraySize;     //Vector size may be different for each process
+        int *vetor = new int[tam_vetor];           //pointer to data vector
+        Node processNode(my_rank, proc_n);          //Discover left and right child process ranks
+
+        if(my_rank != 0)
         {
-            //Node process
-            
-            MPI_Get_count(&Status, MPI_INT, &tam_vetor);  // descubro tamanho da mensagem recebida
-            vetor = new int[tam_vetor];
+            //Não sou raiz, tenho pai
+            MPI_Recv(vetor, tam_vetor, MPI_INT, MPI_ANY_SOURCE, enmTagCommand__SendVector, MPI_COMM_WORLD, &status_pai); 
+            MPI_Get_count(&status_pai, MPI_INT, &tam_vetor);  // descubro tamanho da mensagem recebida
         }
         else
         {
             //Root process
-            tam_vetor = globalFlags::arraySize;    
-            initializeVector(vector, tam_vetor);
-            #ifdef DEBUG
+            initializeVector(vetor, tam_vetor);
             printf("Algoritmo de ordenação selecionado: ");
             if(globalFlags::quickSortSet)
             {
@@ -152,17 +226,17 @@ int main(int argc, char **argv)
             {
                 printf("BubbleSort\n");
             }
-            #endif
+            t1 = MPI_Wtime();        // contagem de tempo inicia neste ponto
         }
 
         // dividir ou conquistar?
-        if ( tam_vetor <= delta )
+        if ( tam_vetor <= globalFlags::delta )
         {
             // conquisto
-            // verifica se deve executar algorítmo bubble ou quick sort
+            //verifica se deve executar algorítmo bubble ou quick sort
             if(globalFlags::quickSortSet)
             {
-                qsort(vetor, tam_vetor, sizeof(int), compare)
+                qsort(vetor, tam_vetor, sizeof(int), compare);
             }
             else
             {
@@ -173,147 +247,38 @@ int main(int argc, char **argv)
         {
             // Dividir
             // quebrar em duas partes e mandar para os filhos
+            int *vetor_esquerda = &vetor[0];
+            int tam_vetor_esquerda = tam_vetor/2;
+            int *vetor_direita = &vetor[tam_vetor_esquerda];
+            int tam_vetor_direita = tam_vetor - tam_vetor_esquerda;
 
-            MPI_Send ( &vetor[0], filho esquerda, tam_vetor/2 );  // mando metade inicial do vetor
-            MPI_Send ( &vetor[tam_vetor/2], filho direita , tam_vetor/2 );  // mando metade final
+            MPI_Send (vetor_esquerda, tam_vetor_esquerda, MPI_INT, processNode.leftChildRank, enmTagCommand__SendVector, MPI_COMM_WORLD);  // mando metade inicial do vetor
+            MPI_Send (vetor_direita, tam_vetor_direita, MPI_INT,processNode.rightChildRank, enmTagCommand__SendVector, MPI_COMM_WORLD);  // mando metade final
         
-            // receber dos filhos            
+            // receber dos filhos
+            MPI_Recv(vetor_esquerda, tam_vetor_esquerda, MPI_INT, processNode.leftChildRank, enmTagCommand__SendVector, MPI_COMM_WORLD, &status_filhos);
+            MPI_Recv(vetor_direita, tam_vetor_direita, MPI_INT, processNode.rightChildRank, enmTagCommand__SendVector, MPI_COMM_WORLD, &status_filhos);        
+
+            //Intercala os vetores recebidos
+            vetor = interleaving(vetor, tam_vetor, tam_vetor_esquerda);
+        }
+
+        if ( my_rank != 0 )
+        {
+            // mando para o pai
+            MPI_Send(vetor, tam_vetor, MPI_INT, status_pai.MPI_SOURCE, enmTagCommand__SendVector, MPI_COMM_WORLD);
+        }
+        else
+        {
+            t2 = MPI_Wtime();                                       // contagem de tempo termina neste ponto
+            DBG_VECTOR(vetor, tam_vetor);                           // sou o raiz, mostro vetor
+            printf("Tempo de execução do algoritmo: %f segundos\n", t2-t1);  // imprime tempo de execução
 
         }
-            
+
+        MPI_Barrier(MPI_COMM_WORLD); //Finish program only when all process close execution
+        MPI_Finalize();                    
     }
-
-    // if(argc < 3)
-    // {
-    //     if(my_rank == 0)
-    //     {
-    //         fprintf(stderr, "Uso:\t%s <tamanho tarefa (vetor)> <numero de tarefas (tamanho saco)> [-qsort]\n", argv[0]);
-    //         result = 1;
-    //     }
-    // }
-    // else 
-    // {
-    //     //Check if quicksort is set
-    //     bool isQuickSortSet(false);
-    //     if(argc >= 4)
-    //     {
-    //         if(strcmp(argv[3], "-qsort") == 0)
-    //         {   
-    //             isQuickSortSet = true;
-    //         }
-    //     }
-    //     //Get work array size and number of tasks
-    //     size_t ARRAY_SIZE = atoi(argv[1]);
-    //     size_t TAREFAS = atoi(argv[2]);
-
-    //     //Create message buffer
-    //     int *message = new int[ARRAY_SIZE];       // Buffer para as mensagens  
-    //     int *pMessage;
-
-    //     //Init master and slave process
-    //     if (my_rank == 0)
-    //     {
-    //         //Master process
-
-    //         //print quicksort flag
-    //         if(isQuickSortSet)
-    //         {
-    //             printf("Algoritmo de ordenação selecionado: Quick Sort\n");
-    //         }
-    //         else
-    //         {
-    //             printf("Algoritmo de ordenação selecionado: Bubble Sort\n");
-    //         }
-    
-
-    //         //Create work stack
-    //         SacoDeTrabalho saco(ARRAY_SIZE, TAREFAS, proc_n);
-
-    //         // printf("Saco de trabalho criado. Valor inicial:\n");
-    //         // saco.printSaco();
-
-    //         //Start clock to measure time
-    //         double t1, t2;
-    //         t1 = MPI_Wtime();        // contagem de tempo inicia neste ponto
-
-    //         // mando o trabalho para os escravos fazerem
-    //         for(int i = 1; (i < saco.m_Proc_n) && (i < saco.m_NumberOfTasks); ++i)
-    //         {
-    //             pMessage = saco.getNextTaskForSlave(i);
-    //             MPI_Send(pMessage, saco.m_ArraySize, MPI_INT, i, enmTagCommand__SendVector, MPI_COMM_WORLD);
-    //         }
-
-            
-    //         MPI_Status status; /* Return status*/
-    //         while(saco.m_NumOfTasksCompleted < saco.m_NumberOfTasks)
-    //         {
-    //             MPI_Probe(MPI_ANY_SOURCE, enmTagCommand__SendVector, MPI_COMM_WORLD, &status);
-    //             saco.setCompletedTaskInStack(&status);  /*MPI_Recv called inside this function*/
-    //             //MPI_Recv(message, saco.m_ArraySize, MPI_INT, MPI_ANY_SOURCE, enmTagCommand__SendVector, MPI_COMM_WORLD, &status);
-    //             //saco.setCompletedTaskInStack(message, status.MPI_SOURCE);
-    //             pMessage = saco.getNextTaskForSlave(status.MPI_SOURCE);
-    //             if(pMessage == NULL)
-    //             {
-    //                 MPI_Send(message, 1, MPI_INT, status.MPI_SOURCE, enmTagCommand__KillProcess, MPI_COMM_WORLD);
-    //             }
-    //             else
-    //             {
-    //                 MPI_Send(pMessage, saco.m_ArraySize, MPI_INT, status.MPI_SOURCE, enmTagCommand__SendVector, MPI_COMM_WORLD);
-    //             }
-    //         }
-
-    //         //Stop clock
-    //         t2 = MPI_Wtime();        // contagem de tempo termina neste ponto
-
-
-    //         //Kill all remaining slaves
-    //         for(int i = 1; i < saco.m_Proc_n; ++i)
-    //             MPI_Send(message, 1, MPI_INT, i, enmTagCommand__KillProcess, MPI_COMM_WORLD);
-
-
-
-    //         //saco shall be sorted at this point
-    //         // printf("Ordenação do saco de trabalho concluído! Resultado:\n");
-    //         // saco.printSaco();  
-            
-    //         //Print time result
-    //         printf("Tempo de execução do algoritmo: %f\n", t2-t1);
-    //     }
-    //     else
-    //     {
-    //         //Slave Process
-    //         MPI_Status status; /* Return status*/
-    //         do
-    //         {
-    //             //Get work from master
-    //             MPI_Recv(message, ARRAY_SIZE, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-    //             if(status.MPI_TAG == enmTagCommand__SendVector)
-    //             {
-    //                 //printf("Processo %d recebeu uma nova tarefa!\n", my_rank);
-    //                 //Sort vector
-    //                 if(isQuickSortSet)
-    //                 {
-    //                     qsort (message, ARRAY_SIZE, sizeof(int), compare);
-    //                 }
-    //                 else
-    //                 {
-    //                     bs(ARRAY_SIZE, message);
-    //                 }
-    //                 //Send vector for master and ask for more work
-    //                 MPI_Send(message, ARRAY_SIZE, MPI_INT, status.MPI_SOURCE, enmTagCommand__SendVector, MPI_COMM_WORLD);
-    //             }
-    //         }while(status.MPI_TAG != enmTagCommand__KillProcess);
-
-    //         //printf("Processo %d cometeu suicidio!\n", my_rank);
-    //     }     
-
-    //     delete(message);
-    // }
-
-    // MPI_Barrier(MPI_COMM_WORLD); //Finish program only when all process close execution
-    // MPI_Finalize();
-
-    //return result;
 
     return 0;
 }
